3bc1f3ce18bf01803184ae5d9ad2e99ac50819b7
Alexandr <francuzby@gmail.com>
2021-08-20 16:46:53 +0300

    @RequestParam

--- src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -1,7 +1,11 @@
 package com.zaurtregulov.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
@@ -16,8 +20,28 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails() {
+//        return "show-emp-details-view";
+//    }
+
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(HttpServletRequest request, Model model) {
+//
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. " + empName;
+//        model.addAttribute("nameAttribute", empName);
+//        model.addAttribute("description", " - udemy instructor");
+//
+//        return "show-emp-details-view";
+//    }
+
     @RequestMapping("/showDetails")
-    public String showEmpDetails() {
+    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model) {
+
+        empName = "Mr. " + empName;
+        model.addAttribute("nameAttribute", empName);
+
         return "show-emp-details-view";
     }
 
--- src/main/webapp/WEB-INF/view/first-view.jsp
+++ src/main/webapp/WEB-INF/view/first-view.jsp
@@ -13,5 +13,10 @@
 </head>
 <body>
 <h2>I wish you luck in Spring MVC learning</h2>
+<br>
+<br>
+
+<a href="askDetails">Please write your details</a>
+
 </body>
 </html>
